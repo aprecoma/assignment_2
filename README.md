fdcf40ab93389f8b3153a7f42f2af4b9732c6c18
IfJay <andreajsm03@gmail.com>
2024-05-18 15:20:08 +0200

    configurazione ambiente close #1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # assignment_2
+
+![coverage](.github/badges/jacoco.svg)
+
+[![Java CI with Maven](https://github.com/aprecoma/assignment_2/actions/workflows/build_on_push.yml/badge.svg?branch=main)](https://github.com/aprecoma/assignment_2/actions/workflows/build_on_push.yml)
---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////
+//  Andrea Precoma              2068227
+//  Andrea Jesus Soranzo Mendez 2075539
+/////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
